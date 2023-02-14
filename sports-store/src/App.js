68d7463c05189cc77dd909b1bd65063d604dfaf0
thinh.bui <thinh.bui@gotecq.com>
2023-02-14 13:35:22 +0700

    update: sports-store

--- sports-store/src/App.js
+++ sports-store/src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import { SportsStoreDataStore } from "./data/DataStore";
+import { Provider } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { ShopConnector } from "./shop/ShopConnector";
 
 export class App extends Component {
   render() {
-    return <div>Setup</div>;
+    return (
+      <Provider store={SportsStoreDataStore}>
+        <Router>
+          <Switch>
+            <Route path="/shop" component={ShopConnector} />
+            <Redirect to="/shop" />
+          </Switch>
+        </Router>
+      </Provider>
+    );
   }
 }
 
--- sports-store/src/shop/ShopConnector.js
+++ sports-store/src/shop/ShopConnector.js
@@ -1,7 +1,55 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Redirect, Switch, Route } from "react-router-dom";
+import { loadData } from "../data/ActionCreators";
+import { DataTypes } from "../data/Types";
+import { Shop } from "./Shop";
 
-export class ShopConnector extends Component {
-  render() {
-    return <div>ShopConnector</div>;
+const mapStateToProps = (dataStore) => ({
+  ...dataStore,
+});
+
+const mapDispatchToProps = {
+  loadData,
+};
+
+const filterProducts = (products = [], category) => {
+  return !category || category === "All"
+    ? products
+    : products.filter(
+        (p) => p.category.toLowerCase() === category.toLowerCase()
+      );
+};
+
+export const ShopConnector = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(
+  class extends Component {
+    render() {
+      return (
+        <Switch>
+          <Route
+            path="/shop/products/:category?"
+            render={(routeProps) => (
+              <Shop
+                {...this.props}
+                {...routeProps}
+                products={filterProducts(
+                  this.props.products,
+                  routeProps.match.params.category
+                )}
+              />
+            )}
+          />
+          <Redirect to="/shop/products" />
+        </Switch>
+      );
+    }
+
+    componentDidMount() {
+      this.props.loadData(DataTypes.CATEGORIES);
+      this.props.loadData(DataTypes.PRODUCTS);
+    }
   }
-}
+);
