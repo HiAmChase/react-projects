9b2ff052603fe1aa78407e4d4665677041eb2fc3
Thinh Bui Ngoc <thinh.bui@gotecq.com>
2023-02-15 22:13:46 +0700

    add: size & property action

--- sports-store/src/shop/ShopConnector.js
+++ sports-store/src/shop/ShopConnector.js
@@ -11,6 +11,7 @@ import {
   clearCart,
 } from "../data/CartActionCreators";
 import { CartDetails } from "./CartDetails";
+import { DataGetter } from "../data/DataGetter";
 
 const mapStateToProps = (dataStore) => ({
   ...dataStore,
@@ -24,13 +25,13 @@ const mapDispatchToProps = {
   clearCart,
 };
 
-const filterProducts = (products = [], category) => {
-  return !category || category === "All"
-    ? products
-    : products.filter(
-        (p) => p.category.toLowerCase() === category.toLowerCase()
-      );
-};
+// const filterProducts = (products = [], category) => {
+//   return !category || category === "All"
+//     ? products
+//     : products.filter(
+//         (p) => p.category.toLowerCase() === category.toLowerCase()
+//       );
+// };
 
 export const ShopConnector = connect(
   mapStateToProps,
@@ -40,17 +41,17 @@ export const ShopConnector = connect(
     render() {
       return (
         <Switch>
+          <Redirect
+            from="/shop/products/:category"
+            to="/shop/products/:category/1"
+            exact={true}
+          />
           <Route
-            path="/shop/products/:category?"
+            path="/shop/products/:category/:page"
             render={(routeProps) => (
-              <Shop
-                {...this.props}
-                {...routeProps}
-                products={filterProducts(
-                  this.props.products,
-                  routeProps.match.params.category
-                )}
-              />
+              <DataGetter {...this.props} {...routeProps}>
+                <Shop {...this.props} {...routeProps} />
+              </DataGetter>
             )}
           />
           <Route
@@ -59,14 +60,14 @@ export const ShopConnector = connect(
               <CartDetails {...this.props} {...routeProps} />
             )}
           />
-          <Redirect to="/shop/products" />
+          <Redirect to="/shop/products/all/1" />
         </Switch>
       );
     }
 
     componentDidMount() {
       this.props.loadData(DataTypes.CATEGORIES);
-      this.props.loadData(DataTypes.PRODUCTS);
+      // this.props.loadData(DataTypes.PRODUCTS);
     }
   }
 );
