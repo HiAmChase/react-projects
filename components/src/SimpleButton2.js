47c2d31c232d2c9d1b6344b46d92db407997d964
thinh.bui <thinh.bui@gotecq.com>
2023-02-21 16:20:21 +0700

    add: components

---
+++ components/src/SimpleButton2.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+export class SimpleButton2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // counter: 0,
+      hasButtonBeenClicked: false,
+    };
+  }
+
+  handleClick = () => {
+    this.props.incrementCallback(5);
+    this.setState({ hasButtonBeenClicked: true });
+    // this.props.callback();
+  };
+
+  render() {
+    return (
+      <button
+        onClick={this.handleClick}
+        className={this.props.className}
+        disabled={
+          this.props.disabled === "true" || this.props.disabled === true
+        }
+      >
+        {this.props.text} {this.props.counter}
+        {this.state.hasButtonBeenClicked && <div>Button Clicked!</div>}
+      </button>
+    );
+  }
+}
--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,22 +1,49 @@
-import React from "react";
-import { CallbackButton } from "./CallbackButton";
+import React, { Component } from "react";
+// import { CallbackButton } from "./CallbackButton";
+// import { SimpleButton } from "./SimpleButton";
+import { SimpleButton2 } from "./SimpleButton2";
+import { HookButton } from "./HookButton";
 
-export function Summary(props) {
-  return (
-    <React.Fragment>
-      <td>{props.index + 1}</td>
-      <td>{props.name}</td>
-      <td>{props.name.length}</td>
-      <td>
-        <CallbackButton callback={props.reverseCallback} />
-      </td>
-      <td>
-        <CallbackButton
-          theme="info"
-          text="Promote"
-          callback={() => props.promoteCallback(props.name)}
-        />
-      </td>
-    </React.Fragment>
-  );
+export class Summary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  incrementCounter = (increment) => {
+    this.setState((state) => {
+      return { counter: state.counter + increment };
+    });
+  };
+
+  render() {
+    const props = this.props;
+    return (
+      <React.Fragment>
+        <td>{props.index + 1}</td>
+        <td>{props.name}</td>
+        <td>{props.name.length}</td>
+        <td>
+          <SimpleButton2
+            className="btn btn-warning btn-sm m-1"
+            callback={props.reverseCallback}
+            text={`Reverse (${props.name})`}
+            counter={this.state.counter}
+            incrementCallback={this.incrementCounter}
+          />
+        </td>
+        <td>
+          <HookButton
+            className="btn btn-info btn-sm m-1"
+            callback={() => props.promoteCallback(props.name)}
+            text={`Promote (${props.name})`}
+            counter={this.state.counter}
+            incrementCallback={this.incrementCounter}
+          />
+        </td>
+      </React.Fragment>
+    );
+  }
 }
